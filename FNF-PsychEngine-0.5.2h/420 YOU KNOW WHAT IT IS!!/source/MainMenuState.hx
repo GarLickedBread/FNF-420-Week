aba61e46f32242692abb2fb99af03eef95768449
GarLickedBread <GBISHOT2@GMAIL.COM>
2023-03-24 20:41:34 -0700

    i did a funny haha

--- FNF-PsychEngine-0.5.2h/420 YOU KNOW WHAT IT IS!!/source/MainMenuState.hx
+++ FNF-PsychEngine-0.5.2h/420 YOU KNOW WHAT IT IS!!/source/MainMenuState.hx
@@ -36,8 +36,10 @@ class MainMenuState extends MusicBeatState
 	public var curbg:FlxSprite;
 	public var curbg2:FlxSprite;
 	public var funny:FlxSprite;
+	public var dewdrop:FlxSprite;
 	public static var psychEngineVersion:String = '0.5.2h'; //This is also used for Discord RPC
-	public static var curSelected:Int = 0;
+	public static var curSelected
+	 = 0;
 
 	var menuItems:FlxTypedGroup<FlxSprite>;
 	private var camGame:FlxCamera;
@@ -62,6 +64,9 @@ class MainMenuState extends MusicBeatState
 	var camFollowPos:FlxObject;
 	var debugKeys:Array<FlxKey>;
 	var char:FlxSprite;
+	var charScoff:FlxSprite;
+	var charGF:FlxSprite;
+	var charGFget:FlxSprite;
 	var rev:FlxSprite;
 	
 
@@ -153,8 +158,18 @@ class MainMenuState extends MusicBeatState
 		funny.antialiasing = ClientPrefs.globalAntialiasing;
 		add(funny);
 
-		menuItems = new FlxTypedGroup<FlxSprite>();
-		add(menuItems);
+
+
+		dewdrop = new FlxSprite().loadGraphic(Paths.image('mainmenu/dewdrop'));
+		dewdrop.scale.set(0.85,0.85);
+		dewdrop.setPosition(-548.15, -364.55);
+		dewdrop.frames = Paths.getSparrowAtlas('mainmenu/dewdrop');//here put the name of the xml
+		dewdrop.animation.addByPrefix('idle', 'bdrop instance ', 24, true);//on 'idle normal' change it to your xml one
+		dewdrop.animation.play('idle');//you can rename the anim however you want to
+		dewdrop.antialiasing = ClientPrefs.globalAntialiasing;
+		add(dewdrop);
+		
+
 
 		var scale:Float = .8;
 		/*if(optionShit.length > 6) {
@@ -162,101 +177,121 @@ class MainMenuState extends MusicBeatState
 		}*/
 
 
+
+		// for (i in 0...optionShit.length)
+		// {
+		// 	var offset:Float = 108 - (Math.max(optionShit.length, 4) - 4) * 80;
+		// 	var menuItem:FlxSprite = new FlxSprite(0, (i * 140)  + offset);
+		// 	menuItem.scale.x = scale;
+		// 	menuItem.scale.y = scale;
+		// 	menuItem.frames = Paths.getSparrowAtlas('mainmenu/menu_' + optionShit[i]);
+		// 	menuItem.animation.addByPrefix('idle', optionShit[i] + " basic", 24);
+		// 	menuItem.animation.addByPrefix('selected', optionShit[i] + " white", 24);
+		// 	menuItem.animation.play('idle');
+		// 	menuItem.ID = i;
+		// 	menuItem.x = 600;
+		// 	//menuItem.y = 900;
+		// 	//menuItem.screenCenter(X);
+		// 	menuItems.add(menuItem);
+		// 	var scr:Float = (optionShit.length - 4) * 0.135;
+		// 	if(optionShit.length < 6) scr = 0;
+		// 	menuItem.scrollFactor.set(0, scr);
+		// 	menuItem.antialiasing = ClientPrefs.globalAntialiasing;
+		// 	menuItem.setGraphicSize(Std.int(menuItem.width * 0.58));
+		// 	menuItem.updateHitbox();
+
+		// 	switch (i)
+		// 	{
+		// 		case 0: //freeplay
+		// 			menuItem.x = 200;
+		// 			menuItem.y = 160;
+		// 			case 1: //storymode
+		// 				menuItem.x = 200;
+		// 				menuItem.y = 299; 
+		// 				case 2: //Credits
+		// 					menuItem.x = 700;
+		// 					menuItem.y = 160;
+		// 					case 3: //options
+		// 						menuItem.x = 700;
+		// 						menuItem.y = 299;
+		// 						case 4: //funny one
+		// 							menuItem.x = 412;
+		// 							menuItem.y = 470;
+		// 							case 5: //funny one
+		// 								menuItem.x = 415;
+		// 								menuItem.y = 900;
+		// 	}
+		// }
+
+
+
+
+
+
 		
-		for (i in 0...optionShit.length)
-		{
-			var offset:Float = 108 - (Math.max(optionShit.length, 4) - 4) * 80;
-			var menuItem:FlxSprite = new FlxSprite(0, (i * 140)  + offset);
-			menuItem.scale.x = scale;
-			menuItem.scale.y = scale;
-			menuItem.frames = Paths.getSparrowAtlas('mainmenu/menu_' + optionShit[i]);
-			menuItem.animation.addByPrefix('idle', optionShit[i] + " basic", 24);
-			menuItem.animation.addByPrefix('selected', optionShit[i] + " white", 24);
-			menuItem.animation.play('idle');
-			menuItem.ID = i;
-			menuItem.x = 600;
-			//menuItem.y = 900;
-			//menuItem.screenCenter(X);
-			menuItems.add(menuItem);
-			var scr:Float = (optionShit.length - 4) * 0.135;
-			if(optionShit.length < 6) scr = 0;
-			menuItem.scrollFactor.set(0, scr);
-			menuItem.antialiasing = ClientPrefs.globalAntialiasing;
-			//menuItem.setGraphicSize(Std.int(menuItem.width * 0.58));
-			menuItem.updateHitbox();
-
-			switch (i)
-			{
-				case 0: //freeplay
-					menuItem.x = 200;
-					menuItem.y = 160;
-					case 1: //storymode
-						menuItem.x = 200;
-						menuItem.y = 299; 
-						case 2: //Credits
-							menuItem.x = 700;
-							menuItem.y = 160;
-							case 3: //options
-								menuItem.x = 700;
-								menuItem.y = 299;
-								case 4: //funny one
-									menuItem.x = 412;
-									menuItem.y = 470;
-									case 5: //funny one
-										menuItem.x = 415;
-										menuItem.y = 900;
-			}
-		}
-
-
-
-
-
-
 		var testgName = 'testguy';
-		var bfmen1 = 'boogy';
 		var bfmen2 = 'anvil';
-		switch (FlxG.random.int(1,3))
+		var bfmen3 = 'lay';
+		var bfmen1 = 'boogy';
+
+		var gfIdle = 'gfDance';
+
+		//BF
+		switch (FlxG.random.int(2,4))
 		{	
 			case 1:
 				char = new FlxSprite(74, 472).loadGraphic(Paths.image('mainmenu/shitheads/menu_' + testgName));//put your cords and image here
 				char.frames = Paths.getSparrowAtlas('mainmenu/shitheads/menu_' + testgName);//here put the name of the xml
 				char.animation.addByPrefix('idle', testgName + ' basic instance ', 24, true);//on 'idle normal' change it to your xml one
 				char.animation.play('idle');//you can rename the anim however you want to
-				char.scrollFactor.set(0, yScroll);
 				char.antialiasing = ClientPrefs.globalAntialiasing;
-				FlxG.sound.play(Paths.sound('appear'), 2);
-			
-				//char.flipX = true;	
+				char.scrollFactor.set(0, yScroll);
 				add(char);
 				case 2:
-					char = new FlxSprite(4.2, 380).loadGraphic(Paths.image('mainmenu/shitheads/menu_' + bfmen1));//put your cords and image here
+					char = new FlxSprite(10.8, 380).loadGraphic(Paths.image('mainmenu/shitheads/menu_' + bfmen1));//put your cords and image here
 					char.frames = Paths.getSparrowAtlas('mainmenu/shitheads/menu_' + bfmen1);//here put the name of the xml
 					char.animation.addByPrefix('idle', bfmen1 + ' basic instance ', 24, true);//on 'idle normal' change it to your xml one
 					char.animation.addByPrefix('death', bfmen1 + ' die basic instance ', 24, false);//on 'idle normal' change it to your xml one
 					char.animation.play('idle');//you can rename the anim however you want to
-					char.scrollFactor.set(0, yScroll);
 					char.antialiasing = ClientPrefs.globalAntialiasing;
-					FlxG.sound.play(Paths.sound('appear'), 2);
-				
-					//char.flipX = true;
+					char.scrollFactor.set(0, yScroll);
 					add(char);	
-					add(char);
 			case 3:
-				char = new FlxSprite(-28.45, 148.4).loadGraphic(Paths.image('mainmenu/shitheads/menu_' + bfmen2));//put your cords and image here
+				char = new FlxSprite(-11.95 + 15, 132 + 20).loadGraphic(Paths.image('mainmenu/shitheads/menu_' + bfmen2));//put your cords and image here
 				char.frames = Paths.getSparrowAtlas('mainmenu/shitheads/menu_' + bfmen2);//here put the name of the xml
 				char.animation.addByPrefix('idle', bfmen2 + ' basic instance ', 24, true);//on 'idle normal' change it to your xml one
 				char.animation.addByPrefix('death', bfmen2 + ' die basic instance ', 24, false);//on 'idle normal' change it to your xml one
 				char.animation.play('idle');//you can rename the anim however you want to
-				char.scrollFactor.set(0, yScroll);
 				char.antialiasing = ClientPrefs.globalAntialiasing;
-				FlxG.sound.play(Paths.sound('appear'), 2);
-			
-				//char.flipX = true;
-				add(char);	
+				char.scrollFactor.set(0, yScroll);
+				add(char);
+				case 4:
+					char = new FlxSprite(4.2, 360 + 26).loadGraphic(Paths.image('mainmenu/shitheads/menu_' + bfmen3));//put your cords and image here
+					char.frames = Paths.getSparrowAtlas('mainmenu/shitheads/menu_' + bfmen3);//here put the name of the xml
+					char.animation.addByPrefix('idle', bfmen3 + ' basic instance ', 24, true);//on 'idle normal' change it to your xml one
+					char.animation.addByPrefix('death', bfmen3 + ' die basic instance ', 24, false);//on 'idle normal' change it to your xml one
+					char.animation.play('idle');//you can rename the anim however you want to
+					char.antialiasing = ClientPrefs.globalAntialiasing;
+					char.scrollFactor.set(0, yScroll);
+					add(char);	
+		}
+		//GF
+		switch (FlxG.random.int(1,1))
+		{	
+			case 1:
+				charGF = new FlxSprite(700, 67).loadGraphic(Paths.image('mainmenu/shitheads/menuGF_' + gfIdle));//put your cords and image here
+				charGF.frames = Paths.getSparrowAtlas('mainmenu/shitheads/menuGF_' + gfIdle);//here put the name of the xml
+				charGF.animation.addByPrefix('idle', gfIdle + ' basic instance ', 24, true);//on 'idle normal' change it to your xml one
+				charGF.animation.addByPrefix('scared', gfIdle + ' Scared basic instance ', 24, false);//on 'idle normal' change it to your xml one
+				charGF.animation.play('idle');//you can rename the anim however you want to
+				charGF.antialiasing = ClientPrefs.globalAntialiasing;
+				charGF.scrollFactor.set(0, yScroll);
+				add(charGF);
 		}
 
-		switch (FlxG.random.int(1,2))
+
+		//Revives
+		switch (FlxG.random.int(1,3))
 		{
 			case 1:
 				//prepare the revive shit
@@ -271,23 +306,85 @@ class MainMenuState extends MusicBeatState
 				add(rev);
 				//lol!
 				case 2:
-								//prepare the revive shit
-								rev = new FlxSprite(-113.25,212.65).loadGraphic(Paths.image('mainmenu/shitheads/revives/revive_2'));
-		
-								rev.frames = Paths.getSparrowAtlas('mainmenu/shitheads/revives/revive_2');//here put the name of the xml
-								rev.animation.addByPrefix('idle', 'revive A basic ', 24, false);//on 'idle normal' change it to your xml one
-								rev.animation.addByPrefix('wait', 'revive wait basic', 24, false);//on 'idle normal' change it to your xml one
-								rev.animation.play('wait');//you can rename the anim however you want to
-								rev.scrollFactor.set(0, yScroll);
-								rev.antialiasing = ClientPrefs.globalAntialiasing;
-								add(rev);
-								//lol!
+					//prepare the revive shit
+					rev = new FlxSprite(-82.2 - 20 ,212.65).loadGraphic(Paths.image('mainmenu/shitheads/revives/revive_2'));
+
+					rev.frames = Paths.getSparrowAtlas('mainmenu/shitheads/revives/revive_2');//here put the name of the xml
+					rev.animation.addByPrefix('idle', 'revive A basic ', 24, false);//on 'idle normal' change it to your xml one
+					rev.animation.addByPrefix('wait', 'revive wait basic', 24, false);//on 'idle normal' change it to your xml one
+					rev.animation.play('wait');//you can rename the anim however you want to
+					rev.scrollFactor.set(0, yScroll);
+					rev.antialiasing = ClientPrefs.globalAntialiasing;
+					add(rev);
+					//lol!
+			case 3:
+				//prepare the revive shit
+				rev = new FlxSprite(-105.35,164.8).loadGraphic(Paths.image('mainmenu/shitheads/revives/revive_3'));
+
+				rev.frames = Paths.getSparrowAtlas('mainmenu/shitheads/revives/revive_3');//here put the name of the xml
+				rev.animation.addByPrefix('idle', 'revive B basic ', 24, false);//on 'idle normal' change it to your xml one
+				rev.animation.addByPrefix('wait', 'revive wait basic', 24, false);//on 'idle normal' change it to your xml one
+				rev.animation.play('wait');//you can rename the anim however you want to
+				rev.scrollFactor.set(0, yScroll);
+				rev.antialiasing = ClientPrefs.globalAntialiasing;
+				add(rev);
+				//lol!
 
 		}
+		 var xoffy:Int = -150; 
+		 var yoffy:Int = 337;
 
+		rev.offset.set(xoffy - 13, yoffy - 23);
+		char.offset.set(xoffy, yoffy - 23);
+		char.scale.set(0.85,0.85);
+		charGF.scale.set(0.85,0.85);
+		menuItems = new FlxTypedGroup<FlxSprite>();
+		add(menuItems);
+		
+		var tex = Paths.getSparrowAtlas('FNF_main_menu_assets');
 
+		for(i in 0...optionShit.length) {
+			var testButton:FlxSprite = new FlxSprite(0, 130);
+			testButton.ID = i;
+			testButton.frames = Paths.getSparrowAtlas('mainmenu/menu_' + optionShit[i]);
+			testButton.animation.addByPrefix('idle', optionShit[i] + " basic", 24);
+			testButton.animation.addByPrefix('selected', optionShit[i] + " white", 24);
+			testButton.animation.play('idle');
+			testButton.antialiasing = true;
+			testButton.scale.set(0.50 ,0.50);
+			testButton.updateHitbox();
+			testButton.screenCenter(X);
+			testButton.scrollFactor.set();
+			
+			// brian was here
 
+			//hi
+			switch(i) {
+				case 0:
+					testButton.setPosition(230, 475);
+				case 1:
+					testButton.setPosition(730, 475);
+				case 2:
+					testButton.setPosition(230, 580);
+				case 3:
+					testButton.setPosition(730, 580);
+				case 4:
+					testButton.alpha = 0;
+					testButton.scale.set(0.34,0.4);
+					testButton.setPosition(252,143.35 + 20);
+					// testButton.x = char.x + xoffy;
+					// testButton.y = char.y + yoffy;
+					//testButton.offset.set(xoffy, yoffy);
+				case 5:
+					testButton.setPosition(630, 640);
 
+			}
+			menuItems.add(testButton);
+		}		
+
+		add(menuItems);
+
+		
 
 
 
@@ -358,6 +455,7 @@ class MainMenuState extends MusicBeatState
 		}
 		
 		timerThing += elapsed;
+
 		funny.alpha = Math.sin(timerThing) + 0.1;
 		menuItems.forEach(function(spr:FlxSprite)
 				{
@@ -390,14 +488,15 @@ class MainMenuState extends MusicBeatState
 											case 2:
 											FlxG.sound.play(Paths.sound('introGo-pixel'));		
 									}
-									char.animation.play('death');//you can rename the anim however you want to			
+									char.animation.play('death');//you can rename the anim however you want to	
+									charGF.animation.play('scared');		
 									//FlxFlicker.flicker(magenta, 1.1, 0.15, false);
 									new FlxTimer().start(2.3, function(tmr:FlxTimer)
 										{
 											rev.animation.play('idle');
 											FlxG.sound.play(Paths.sound('revive'));
 											char.animation.play('idle');
-
+											charGF.animation.play('idle');
 
 
 
@@ -420,8 +519,8 @@ class MainMenuState extends MusicBeatState
 		//var lerpVal:Float = CoolUtil.boundTo(elapsed * 7.5, 0, 1);
 		//camFollowPos.setPosition(FlxMath.lerp(camFollowPos.x, camFollow.x, lerpVal), FlxMath.lerp(camFollowPos.y, camFollow.y, lerpVal));
 
-		// if (!selectedSomethin)
-		// 	{	
+		 if (!selectedSomethin)
+			{	
 		// 		canClick = false;
 		// 		if (controls.UI_UP_P)
 		// 		{	
@@ -499,15 +598,20 @@ class MainMenuState extends MusicBeatState
 		// 						changeItem(-2);
 		// 					}
 		// 			}
-				// if (controls.BACK)
-				// {	
-					
-				// 	rev.animation.play('idle');
-				// 		FlxG.sound.play(Paths.sound('revive'));
-				// 		char.animation.play('idle');
-				// 		fuckerDead == false;
+				if (controls.BACK)
+				{	
+					// FlxG.camera.fade(FlxColor.BLACK, 0.7, false);
+					// FlxTween.tween(spr, {alpha: 0}, 1.3, {
+					// 	ease: FlxEase.quadOut,
+					// 	onComplete: function(twn:FlxTween)
+					// 	{
+					// 		spr.kill();
+					// 	}
+					// });
+				FlxG.sound.play(Paths.sound('cancelMenu'));
+				MusicBeatState.switchState(new MainMenuState());
 	
-				// }
+				}
 	
 		// 		if (controls.ACCEPT)
 		// 		{
@@ -592,7 +696,7 @@ class MainMenuState extends MusicBeatState
 		// 			MusicBeatState.switchState(new MasterEditorMenu());
 		// 		}
 		// 		#end
-		// 	}
+			}
 
 		#if windows
 		if (curbg != null)
@@ -673,13 +777,13 @@ class MainMenuState extends MusicBeatState
 					FlxG.switchState(new StoryMenuState());
 					trace("Story Menu Selected");
 				case 'freeplay':
-					FlxG.switchState(new StoryMenuState());
+					FlxG.switchState(new FreeplayState());
 					trace("Freeplay Menu Selected");
 				case 'options':
-					FlxG.switchState(new StoryMenuState());
+					LoadingState.loadAndSwitchState(new options.OptionsState());
 					trace("Options Menu Selected");
 				case 'credits':
-					FlxG.switchState(new StoryMenuState());
+					FlxG.switchState(new CreditsState());
 					trace("Gallery Menu Selected");
 			}		
 		}
--- FNF-PsychEngine-0.5.2h/420 YOU KNOW WHAT IT IS!!/source/PlayState.hx
+++ FNF-PsychEngine-0.5.2h/420 YOU KNOW WHAT IT IS!!/source/PlayState.hx
@@ -2408,7 +2408,17 @@ class PlayState extends MusicBeatState
 			openChartEditor();
 		}
 
-		// FlxG.watch.addQuick('VOL', vocals.amplitudeLeft);
+		// FlxG.watch.addQuick('VOL', vocals.amplitudeLeft);  
+		iconP1.centerOffsets();
+		iconP2.centerOffsets();
+	  
+		iconP1.updateHitbox();
+		iconP2.updateHitbox();
+	  
+		var iconOffset:Int = 26;
+	  
+		iconP1.x = healthBar.x + (healthBar.width * (FlxMath.remapToRange(healthBar.percent, 0, 100, 100, 0) * 0.01) - iconOffset);
+		iconP2.x = healthBar.x + (healthBar.width * (FlxMath.remapToRange(healthBar.percent, 0, 100, 100, 0) * 0.01)) - (iconP2.width - iconOffset);
 		// FlxG.watch.addQuick('VOLRight', vocals.amplitudeRight);
 
 		var mult:Float = FlxMath.lerp(1, iconP1.scale.x, CoolUtil.boundTo(1 - (elapsed * 9), 0, 1));
@@ -2422,18 +2432,22 @@ class PlayState extends MusicBeatState
 		var iconOffset:Int = 26;
 
 		iconP1.x = healthBar.x + (healthBar.width * (FlxMath.remapToRange(healthBar.percent, 0, 100, 100, 0) * 0.01)) + (150 * iconP1.scale.x - 150) / 2 - iconOffset;
-		iconP2.x = healthBar.x + (healthBar.width * (FlxMath.remapToRange(healthBar.percent, 0, 100, 100, 0) * 0.01)) - (150 * iconP2.scale.x) / 2 - iconOffset * 2;
+		iconP2.x = healthBar.x + (healthBar.width * (FlxMath.remapToRange(healthBar.percent, 0, 100, 100, 0) * 0.01)) - (150 * iconP2.scale.x) / 2 - iconOffset * 2 ;
 
 		if (health > 2)
 			health = 2;
 
-		if (healthBar.percent < 20)
+		if (healthBar.percent < 35)
 			iconP1.animation.curAnim.curFrame = 1;
+		else if (healthBar.percent > 65)
+			iconP1.animation.curAnim.curFrame = 2;
 		else
 			iconP1.animation.curAnim.curFrame = 0;
 
-		if (healthBar.percent > 80)
+		if (healthBar.percent > 75)
 			iconP2.animation.curAnim.curFrame = 1;
+		else if (healthBar.percent < 45)
+			iconP2.animation.curAnim.curFrame = 2;
 		else
 			iconP2.animation.curAnim.curFrame = 0;
 
@@ -4333,6 +4347,31 @@ class PlayState extends MusicBeatState
 		{
 			gf.dance();
 		}
+
+		  
+		  if (curBeat % gfSpeed == 0) {
+			curBeat % (gfSpeed * 2) == 0 ? {
+			 iconP1.scale.set(1.1, 0.8);
+			 iconP2.scale.set(1.1, 1.3);
+		 
+			 FlxTween.angle(iconP1, -15, 0, Conductor.crochet / 1300 * gfSpeed, {ease: FlxEase.quadOut});
+			 FlxTween.angle(iconP2, 15, 0, Conductor.crochet / 1300 * gfSpeed, {ease: FlxEase.quadOut});
+			} : {
+			 iconP1.scale.set(1.1, 1.3);
+			 iconP2.scale.set(1.1, 0.8);
+		 
+			 FlxTween.angle(iconP2, -15, 0, Conductor.crochet / 1300 * gfSpeed, {ease: FlxEase.quadOut});
+			 FlxTween.angle(iconP1, 15, 0, Conductor.crochet / 1300 * gfSpeed, {ease: FlxEase.quadOut});
+			}
+		 
+			FlxTween.tween(iconP1, {'scale.x': 1, 'scale.y': 1}, Conductor.crochet / 1250 * gfSpeed, {ease: FlxEase.quadOut});
+			FlxTween.tween(iconP2, {'scale.x': 1, 'scale.y': 1}, Conductor.crochet / 1250 * gfSpeed, {ease: FlxEase.quadOut});
+		 
+			iconP1.updateHitbox();
+			iconP2.updateHitbox();
+		   } 
+
+
 		if (curBeat % boyfriend.danceEveryNumBeats == 0 && boyfriend.animation.curAnim != null && !boyfriend.animation.curAnim.name.startsWith('sing') && !boyfriend.stunned)
 		{
 			boyfriend.dance();
