4b1d0174cda2258b9808e3972539b71ef1ba13f4
GarLickedBread <GBISHOT2@GMAIL.COM>
2023-03-26 01:03:00 -0700

    icons dont rotate when health is low lol

--- FNF-PsychEngine-0.5.2h/420 YOU KNOW WHAT IT IS!!/source/PlayState.hx
+++ FNF-PsychEngine-0.5.2h/420 YOU KNOW WHAT IT IS!!/source/PlayState.hx
@@ -4348,20 +4348,20 @@ class PlayState extends MusicBeatState
 			gf.dance();
 		}
 
-		  
+		var heheho:Float = (healthBar.percent * 0.01) + 0.01;
 		  if (curBeat % gfSpeed == 0) {
 			curBeat % (gfSpeed * 2) == 0 ? {
 			 iconP1.scale.set(1.1, 0.8);
 			 iconP2.scale.set(1.1, 1.3);
-		 
-			 FlxTween.angle(iconP1, SONG.bpm * -0.09, 0, Conductor.crochet / 1300 * gfSpeed, {ease: FlxEase.quadOut});
-			 FlxTween.angle(iconP2, SONG.bpm * 0.09, 0, Conductor.crochet / 1300 * gfSpeed, {ease: FlxEase.quadOut});
+				
+			 FlxTween.angle(iconP1, SONG.bpm * -0.09 * heheho, 0, Conductor.crochet / 1300 * gfSpeed, {ease: FlxEase.quadOut});
+			 FlxTween.angle(iconP2, SONG.bpm * 0.09 * heheho, 0, Conductor.crochet / 1300 * gfSpeed, {ease: FlxEase.quadOut});
 			} : {
 			 iconP1.scale.set(1.1, 1.3);
 			 iconP2.scale.set(1.1, 0.8);
 		 
-			 FlxTween.angle(iconP2, SONG.bpm * -0.09, 0, Conductor.crochet / 1300 * gfSpeed, {ease: FlxEase.quadOut});
-			 FlxTween.angle(iconP1, SONG.bpm * 0.09, 0, Conductor.crochet / 1300 * gfSpeed, {ease: FlxEase.quadOut});
+			 FlxTween.angle(iconP2, SONG.bpm * -0.09 * heheho, 0, Conductor.crochet / 1300 * gfSpeed, {ease: FlxEase.quadOut});
+			 FlxTween.angle(iconP1, SONG.bpm * 0.09 * heheho, 0, Conductor.crochet / 1300 * gfSpeed, {ease: FlxEase.quadOut});
 			}
 		 
 			FlxTween.tween(iconP1, {'scale.x': 1, 'scale.y': 1}, Conductor.crochet / 1250 * gfSpeed, {ease: FlxEase.quadOut});
